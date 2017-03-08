5d14f692f499c93f845a0e95dde8f1f1c817699a
André Gonçalves Rodrigues <andre.goncalves.rodrigues@gmail.com>
2017-03-07 22:13:53 -0300

    correções realizadas para a primeira fase do projeto

--- src/AGR/Elements/FieldFacade.php
+++ src/AGR/Elements/FieldFacade.php
@@ -13,37 +13,35 @@ class FieldFacade{
     public function createField($element){
         switch($element['class']){
             case 'textarea':
-                echo "<textarea name=\"{$element['name']}\" rows=\"{$element['rows']}\" cols=\"{$element['cols']}\">{$element['content']}</textarea><br><br>\n";
+                echo "<textarea name=\"{$element['name']}\" rows=\"{$element['rows']}\" cols=\"{$element['cols']}\">{$element['content']}</textarea><br><br>";
                 break;
             
             case 'input':
-                echo "<input type=\"{$element['type']}\" name=\"{$element['name']}\" value=\"{$element['value']}\" /><br>";
+                echo "<input type=\"{$element['type']}\" name=\"{$element['name']}\" value=\"{$element['value']}\"/><br>";
                 break;
             
             case 'text':
-                echo "{$element['content']}<br>\n";
+                echo "{$element['content']}<br>";
                 break;
 
             case 'fieldset':
-                echo "<fieldset><br>
-                        <center>\n";
+                echo "<fieldset><br><center>";
                 break;
             
             case 'endFieldset':
-                echo "  </center><br>
-                     </fieldset><br>\n";
+                echo "</center><br></fieldset><br>";
                 break;
 
             case 'ul':
-                echo "<ul><br>\n";
+                echo "<ul><br>";
                 break;
             
              case 'li':
-                echo "<li>{$element['value']}</li><br>\n";
+                echo "<li>{$element['value']}</li><br>";
                 break;
 
             case 'endul':
-                echo "</ul><br>\n";
+                echo "</ul><br>";
                 break;
 
             case 'select':
@@ -59,8 +57,12 @@ class FieldFacade{
     }
 
     public function populate($dados, Validator $validator){
-        $this->setErrors($validator->validate($dados, $this->elements));
-        $this->populateFields($dados);
+        $errors = $validator->validate($dados, $this->elements);
+        $this->setErrors($errors);
+        
+        if(!isset($errors) || count($errors) == 0)
+            $this->populateFields($dados);
+        
         return $this->elements;
     }
 
--- tests/src/AGR/Elements/FormTest.php
+++ tests/src/AGR/Elements/FormTest.php
@@ -40,8 +40,63 @@ class FormTest extends \PHPUnit_Framework_TestCase {
 
         $form = new Form("/produto", "produto",new \AGR\Validator\Validator(new \AGR\Request\Request()));
         $form->populate($dados);
+
+        $this->expectOutputString('<form action="/produto" method="produto"><ul><br></ul><br><fieldset><br><center>Nome:<br><input type="text" name="nome" value="produto teste"/><br>Valor:<br><input type="numeric" name="valor" value="100.00"/><br>Descrição:<br><textarea name="descricao" rows="10" cols="40">produto para vendas de teste</textarea><br><br><select><br><option value=\'categoria1\'>1</option><br><option value=\'categoria2\'>2</option><br></select></center><br></fieldset><br></form>');
+
+        $form->render();
     }
 
+    public function testValidateMissingNomeMethod(){     
+        $dados = array();
+
+        $form = new Form("/produto", "produto",new \AGR\Validator\Validator(new \AGR\Request\Request()));
+        $form->populate($dados);
+
+        $this->expectOutputString('<form action="/produto" method="produto"><ul><br><li>nome não inicializado!</li><br><li>valor não inicializado!</li><br></ul><br></form>');
+
+        $form->render();
+    }
+
+     public function testValidateNomeTooLongMethod(){     
+        $dados = array(
+            'nome'=>'produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste',
+            'valor'=>'100.00',
+        );
+
+        $form = new Form("/produto", "produto",new \AGR\Validator\Validator(new \AGR\Request\Request()));
+        $form->populate($dados);
+
+        $this->expectOutputString('<form action="/produto" method="produto"><ul><br><li>nome maior que 200 caracteres!</li><br></ul><br></form>');
+
+        $form->render();
+    }
+
+     public function testValidateMissingValorMethod(){     
+        $dados = array(
+            'nome'=>'nome',
+        );
+
+        $form = new Form("/produto", "produto",new \AGR\Validator\Validator(new \AGR\Request\Request()));
+        $form->populate($dados);
+
+        $this->expectOutputString('<form action="/produto" method="produto"><ul><br><li>valor não inicializado!</li><br></ul><br></form>');
+
+        $form->render();
+    }
+
+    public function testValidateValorInvalidMethod(){     
+        $dados = array(
+            'nome'=>'nome',
+            'valor'=>'a',
+        );
+
+        $form = new Form("/produto", "produto",new \AGR\Validator\Validator(new \AGR\Request\Request()));
+        $form->populate($dados);
+
+        $this->expectOutputString('<form action="/produto" method="produto"><ul><br><li>valor não é numérico!</li><br></ul><br></form>');
+
+        $form->render();
+    }
 }
 
 
