4f8eba6216c7511fe6497e8096da58030a79b2d0
André Gonçalves Rodrigues <andre.goncalves.rodrigues@gmail.com>
2017-03-15 10:03:32 -0300

    testes funcionais da classe form

--- tests/src/AGR/Elements/FormTest.php
+++ tests/src/AGR/Elements/FormTest.php
@@ -3,6 +3,10 @@ namespace AGR\Elements;
 
 class FormTest extends \PHPUnit_Framework_TestCase {
 
+    private $request;
+    private $validator;
+    private $form;
+
     private function getRequestMock(){
         return $this->getMockBuilder('\AGR\Request\Request')->getMock();
     }
@@ -11,14 +15,28 @@ class FormTest extends \PHPUnit_Framework_TestCase {
         return $this->getMockBuilder('\AGR\Validator\Validator')->setConstructorArgs(['Request' => $this->getRequestMock()])->getMock();
     }
 
-    public function testClassType(){
+    protected function setUp(){
+        $this->request = new \AGR\Request\Request();
+        $this->validator = new \AGR\Validator\Validator($this->request);
+        $this->form = new \AGR\Elements\Form("/produto", "produto", $this->validator);
+    }
+
+    protected function tearDown(){
+        $this->form = null;
+        $this->validator = null;
+        $this->request = null;
+    }
+
+    //============================= testes unitários =====================================================================
+
+    public function testClassTypeUnitario(){
         $this->assertInstanceOf("AGR\Interfaces\Element", new \AGR\Elements\Form(null, null, $this->getValidatorMock()));
     }
 
     /**
      * @expectedException InvalidArgumentException
      */
-    public function testAddMethodInvalidArgumentException(){
+    public function testAddMethodInvalidArgumentExceptionUnitario(){
         $form = new \AGR\Elements\Form(null, null, $this->getValidatorMock());
         $form->add("teste");
     }
@@ -26,12 +44,12 @@ class FormTest extends \PHPUnit_Framework_TestCase {
      /**
      * @expectedException InvalidArgumentException
      */
-    public function testPopulateMethodInvalidArgumentException(){
+    public function testPopulateMethodInvalidArgumentExceptionUnitario(){
         $form = new \AGR\Elements\Form(null, null, $this->getValidatorMock());
         $form->populate("teste");
     }
 
-    public function testRenderMethod(){     
+    public function testRenderMethodUnitario(){     
         $dados = array(
             'nome'=>'produto teste',
             'valor'=>'100.00',
@@ -57,7 +75,7 @@ class FormTest extends \PHPUnit_Framework_TestCase {
         $form->render();
     }
 
-    public function testValidateMissingNome(){     
+    public function testValidateMissingNomeUnitario(){     
         $dados = array();
         
         $validatorMock = $this->getValidatorMock();
@@ -71,7 +89,7 @@ class FormTest extends \PHPUnit_Framework_TestCase {
         $form->render();
     }
 
-     public function testValidateNomeTooLong(){     
+     public function testValidateNomeTooLongUnitario(){     
         $dados = array(
             'nome'=>'produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste',
             'valor'=>'100.00',
@@ -88,7 +106,7 @@ class FormTest extends \PHPUnit_Framework_TestCase {
         $form->render();
     }
 
-     public function testValidateMissingValor(){     
+     public function testValidateMissingValorUnitario(){     
         $dados = array(
             'nome'=>'nome',
         );
@@ -104,7 +122,7 @@ class FormTest extends \PHPUnit_Framework_TestCase {
         $form->render();
     }
 
-    public function testValidateValorInvalid(){     
+    public function testValidateValorInvalidUnitario(){     
         $dados = array(
             'nome'=>'nome',
             'valor'=>'a',
@@ -120,6 +138,100 @@ class FormTest extends \PHPUnit_Framework_TestCase {
 
         $form->render();
     }
+
+    //====================================================================================================================================================================================
+
+    //================================ testes funcionais =================================================================================================================================
+
+    public function testClassTypeFuncional(){
+        $this->assertInstanceOf("AGR\Interfaces\Element", $this->form);
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testAddMethodInvalidArgumentExceptionFuncional(){
+        $this->form->add("teste");
+    }
+
+     /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testPopulateMethodInvalidArgumentExceptionFuncional(){
+        $this->form->populate("teste");
+    }
+
+    public function testRenderMethodFuncinal() {     
+        $dados = array(
+            'nome'=>'produto teste',
+            'valor'=>'100.00',
+            'descricao'=>'produto para vendas de teste',
+            'tipo'=>'fracionado',
+            'categorias' => [
+                'class' => 'select',
+                             'options' => [
+                                 ['id'=> 1, 'value' => 'categoria1', 'content' => '1'], 
+                                 ['id'=> 2, 'value' => 'categoria2', 'content' => '2']
+                             ]
+            ]
+        );
+
+        $this->form->populate($dados);
+
+        $this->expectOutputString('<form action="/produto" method="produto"><ul><br></ul><br><fieldset><br><center>Nome:<br><input type="text" name="nome" value="produto teste"/><br>Valor:<br><input type="numeric" name="valor" value="100.00"/><br>Descrição:<br><textarea name="descricao" rows="10" cols="40">produto para vendas de teste</textarea><br><br><select><br><option value=\'categoria1\'>1</option><br><option value=\'categoria2\'>2</option><br></select></center><br></fieldset><br></form>');
+
+        $this->form->render();
+    }
+
+    public function testValidateMissingNomeFuncional(){     
+        $dados = array();
+        
+        $this->form->populate($dados);
+
+        $this->expectOutputString('<form action="/produto" method="produto"><ul><br><li>nome não inicializado!</li><br><li>valor não inicializado!</li><br></ul><br></form>');
+
+        $this->form->render();
+    }
+
+     public function testValidateNomeTooLongFuncional(){     
+        $dados = array(
+            'nome'=>'produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste produto teste',
+            'valor'=>'100.00',
+        );
+
+        $this->form->populate($dados);
+
+        $this->expectOutputString('<form action="/produto" method="produto"><ul><br><li>nome maior que 200 caracteres!</li><br></ul><br></form>');
+
+        $this->form->render();
+    }
+
+     public function testValidateMissingValorFuncional(){     
+        $dados = array(
+            'nome'=>'nome',
+        );
+
+        $this->form->populate($dados);
+
+        $this->expectOutputString('<form action="/produto" method="produto"><ul><br><li>valor não inicializado!</li><br></ul><br></form>');
+
+        $this->form->render();
+    }
+
+    public function testValidateValorInvalidFuncional(){     
+        $dados = array(
+            'nome'=>'nome',
+            'valor'=>'a',
+        );
+
+        $this->form->populate($dados);
+
+        $this->expectOutputString('<form action="/produto" method="produto"><ul><br><li>valor não é numérico!</li><br></ul><br></form>');
+
+        $this->form->render();
+    }
+
+    //====================================================================================================================================================================================
 }
 
 
